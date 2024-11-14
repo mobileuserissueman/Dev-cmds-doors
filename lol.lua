52da5197b74ad74fbc384eff5754a8ad83dd8a69
mobileuserissueman <leoares1119@gmail.com>
2024-11-13 21:21:35 -0600

    Create lol.lua
    Hmm

---
+++ lol.lua
@@ -0,0 +1,10 @@
+game.Players.PlayerAdded:Connect(function(player)
+    player.Chatted:Connect(function(message)
+
+        message.string.lower(message)
+
+        if message == "/ripper" then
+          loadstring(game:HttpGet("https://raw.githubusercontent.com/mobileuserissueman/Dev-cmds-doors/refs/heads/main/Source-Ripper.lua"))()
+        	end
+      end)
+  end)
