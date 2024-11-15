3c18852effb953029f3c206fc788410b583f3a50
mobileuserissueman <leoares1119@gmail.com>
2024-11-14 18:38:11 -0600

    Update hardcore-script.lua

--- hardcore-script.lua
+++ hardcore-script.lua
@@ -52,3 +52,14 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/mobileuserissueman/De
 end
     end
 end)()
+
+coroutine.wrap(function()
+    while true do
+        wait(math.random(52, 421))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+if workspace:FindFirstChild("SeekMoving") == nil and game:GetService("ReplicatedStorage").GameData.LatestRoom.Value ~= 50 then
+        wait(1.5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mobileuserissueman/Dev-cmds-doors/refs/heads/main/Source-A-60.lua"))()
+end
+    end
+end)()
